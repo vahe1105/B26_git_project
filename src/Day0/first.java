2fcde7c0157489bfdedb24239e42fa12703608af
Narke <nareka515@gmail.com>
2022-03-26 13:10:21 -0700

    wrot the code

--- src/Day0/first.java
+++ src/Day0/first.java
@@ -2,7 +2,10 @@ package Day0;
 
 public class first {
 
-    // TODO: Works on this
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("working...");
+    }
 
     // extra line 1
     // extra line 2
